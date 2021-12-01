9da90b1c48d6030f9978edf83815a920a6fe9319
Dunja Gojković <gojkovic.ra37.2018@uns.ac.rs>
2021-12-01 14:23:59 +0100

    initial profile page  design

--- IsaFrontApp/src/app/app.module.ts
+++ IsaFrontApp/src/app/app.module.ts
@@ -1,19 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { LoginComponent } from './login/login.component';
+import { CottageProfilesClientComponent } from './cottage-profiles-client/cottage-profiles-client.component';
+import { HomepageClientComponent } from './homepage-client/homepage-client.component';
+import { BoatProfilesClientComponent } from './boat-profiles-client/boat-profiles-client.component';
+import { InstructorProfilesClientComponent } from './instructor-profiles-client/instructor-profiles-client.component';
 
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
-import { HomepageClientComponent } from './homepage-client/homepage-client.component';
+import {MatButtonModule} from '@angular/material/button';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 @NgModule({
   declarations: [
@@ -21,19 +29,26 @@ import { HomepageClientComponent } from './homepage-client/homepage-client.compo
     RegistrationComponent,
     FrontPageComponent,
     LoginComponent,
-    HomepageClientComponent
+    HomepageClientComponent,
+    CottageProfilesClientComponent,
+    BoatProfilesClientComponent,
+    InstructorProfilesClientComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NoopAnimationsModule,
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
 })
 export class AppModule { }
