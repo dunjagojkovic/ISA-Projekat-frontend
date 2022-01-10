08703cf78bcd1f8d3b7162131494e71e33d05a6b
Dunja Gojković <gojkovic.ra37.2018@uns.ac.rs>
2022-01-10 13:20:37 +0100

    canceling boats and instructors

--- IsaFrontApp/src/app/homepage-client/reservations-client/reservations-client.component.ts
+++ IsaFrontApp/src/app/homepage-client/reservations-client/reservations-client.component.ts
@@ -54,9 +54,43 @@ export class ReservationsClientComponent implements OnInit {
 
    
     
-  cancel(id: number){
+  cancelHouse(id: number){
     this.api.cancelHouseReservation(id).subscribe((response:any) => {      {
       this.houseReservations = response;
+      console.log(response);
+      if(response == true){
+       // this.houseReservations = this.houseReservations.filter((e:any) => e.id != id);  
+       this._snackBar.open('You have successfully canceled your reservation. ', 'Close', {duration: 5000});   
+       window.location.reload();
+         } else if (response == false){
+          this._snackBar.open('Cannot cancel. ', 'Close', {duration: 3000});       
+          //alert('Cannot cancel.')   
+         }
+
+    }
+    });
+  }
+
+  cancelBoat(id: number){
+    this.api.cancelBoatReservation(id).subscribe((response:any) => {      {
+      this.boatReservations = response;
+      console.log(response);
+      if(response == true){
+       // this.houseReservations = this.houseReservations.filter((e:any) => e.id != id);  
+       this._snackBar.open('You have successfully canceled your reservation. ', 'Close', {duration: 5000});   
+       window.location.reload();
+         } else if (response == false){
+          this._snackBar.open('Cannot cancel. ', 'Close', {duration: 3000});       
+          //alert('Cannot cancel.')   
+         }
+
+    }
+    });
+  }
+
+  cancelInstructor(id: number){
+    this.api.cancelInstructorReservation(id).subscribe((response:any) => {      {
+      this.instructorReservations = response;
       console.log(response);
       if(response == true){
        // this.houseReservations = this.houseReservations.filter((e:any) => e.id != id);  
