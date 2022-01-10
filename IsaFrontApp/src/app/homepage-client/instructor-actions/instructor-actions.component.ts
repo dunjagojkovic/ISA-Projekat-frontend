a9e2ee723c5c953247c2096620025fa498b32329
Dunja Gojković <gojkovic.ra37.2018@uns.ac.rs>
2022-01-10 13:08:00 +0100

    book instructors on action

--- IsaFrontApp/src/app/homepage-client/instructor-actions/instructor-actions.component.ts
+++ IsaFrontApp/src/app/homepage-client/instructor-actions/instructor-actions.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { MatSnackBar} from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-instructor-actions',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstructorActionsComponent implements OnInit {
 
-  constructor() { }
+  actions = [] as any; 
+  todayDate: Date = new Date();
+  instructor: any = {} as any; 
+
+  constructor(
+    private router: Router,
+    private api: ApiService,   
+    private _snackBar: MatSnackBar
+  ) {  }
 
   ngOnInit(): void {
+      this.api.getAllInstructorsOnActions().subscribe((response: any) => {
+        this.actions = response;
+        console.log(response);
+        });
+
   }
+
+  onSubmit(id:any) {
+
+    let action: any;
+
+    for(let a of this.actions) {
+        if(a.id === id) {
+          action = a;
+        }
+    }
+    let data = {
+      name:  action.adventureProfile.name,
+      extraServices: action.adventureProfile.extraServices,
+      startDate:  action.startDate,
+      endDate: action.endDate,
+      adventureId:  action.adventureProfile.id,
+      pricelist: action.actionPrice
+    }  
+
+    this.api.bookInstructor(data).subscribe((response: any) => {
+        console.log(response);
+        if(response != null){
+          this.instructor = response;
+          this.router.navigate(['/reservations-client']);
+          this._snackBar.open('Thank you for your reservation', 'Close');
+        }
+        if(response == null){
+          this._snackBar.open('Sorry the instructor is booked in the meantime.  If you are flexible, check out some alternative dates.', 'Close', {duration: 5000})
+        } 
+       
+    });
+
+}
 
 }
